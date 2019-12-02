1e93be124633f672023d9747289e3147ccae0b66
Alec Larson <alec.stanford.larson@gmail.com>
2019-12-02 14:39:37 -0500

    1.0.0

---
+++ types.d.ts
@@ -0,0 +1,20 @@
+export * from './react'
+
+/** For solving generic types */
+export type Solve<T> = T
+
+/** Try to simplify `&` out of an object type */
+export type Remap<T> = { [P in keyof T]: T[P] }
+
+export type Pick<T, K extends keyof T> = Solve<{ [P in K]: T[P] }>
+
+export type Partial<T> = Solve<{ [P in keyof T]?: T[P] | undefined }>
+
+export type Overwrite<T, U> = Solve<U & Omit<T, keyof U>>
+
+export type MergeUnknown<T, U> = Solve<T & Omit<U, keyof T>>
+
+export type MergeDefaults<T extends object, U extends Partial<T>> = Remap<
+  Pick<T, Exclude<keyof T, keyof U>> &
+    Partial<Pick<T, Extract<keyof U, keyof T>>>
+>
