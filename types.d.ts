15c96a75a89d2a564448edc8eee81a196cbd88b3
Alec Larson <alec.stanford.larson@gmail.com>
2020-03-23 15:27:48 -0400

    feat: add AnyFn type

--- types.d.ts
+++ types.d.ts
@@ -45,3 +45,7 @@ export interface UnknownProps extends UniformProps<unknown> {}
 export class Any {
   private _: never
 }
+
+export type AnyFn<In extends ReadonlyArray<any> = any[], Out = any> = (
+  ...args: In
+) => Out
